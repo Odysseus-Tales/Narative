6cc880fe19d73a36eb4d4924850384e44d1626da
Richard Marcoux <marcourp@morrisville.edu>
2020-02-11 10:36:55 -0500

    Update Text Component

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]  Text txtComponent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtComponent.text = ("I am added programmatically!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
