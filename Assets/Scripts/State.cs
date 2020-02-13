99fdcf28341288a36a045a56d0d51241a76d9d41
Richard Marcoux <marcourp@morrisville.edu>
2020-02-13 10:50:37 -0500

    Creating an Array

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [TextArea(14,10)]  [SerializeField] string strStoryText; 
+    [TextArea(14,10)]  [SerializeField] string strStoryText;
+    [SerializeField] State[] nextState;
 
     public string GetStateStory()
     {
         return strStoryText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextState;
     }
 }
