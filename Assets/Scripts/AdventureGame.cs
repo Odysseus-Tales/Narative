73f4d080966c2b3bbc6739982233f13e28b229c2
Richard Marcoux <marcourp@morrisville.edu>
2020-02-13 12:41:57 -0500

    Manage Next States

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureGame : MonoBehaviour
 {
-    [SerializeField]  Text txtComponent;
+    [SerializeField] Text txtComponent;
     [SerializeField] State startingState;
 
     // our current state
@@ -19,6 +20,26 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
     }
-}
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextState();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextState[2];
+        }
+
+        txtComponent.text = state.GetStateStory();
+    } // ManageState
+}  // AdventureGame
